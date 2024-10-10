2157a5eb84a54d22d99d8f1ae89e921f82098edd
hansa-surge <hansa@surge.global>
2024-10-10 13:32:04 +0530

    added screen to final 04

--- src/part_04/final_file.py
+++ src/part_04/final_file.py
@@ -1,10 +1,25 @@
-from screens import Screen
-from containers import Container, ContainerManager
-from items import ItemManager
 import csv
 import copy
 from typing import List
-from items import Item
+
+class Screen:
+    def display_menu(self):
+        raise NotImplementedError("This method should be overridden by subclasses.")
+
+    def get_choice(self):
+        return input("Your choice: ")
+
+    def handle_choice(self, choice):
+        raise NotImplementedError("This method should be overridden by subclasses.")
+
+    def run(self):
+        while True:
+            self.display_menu()
+            choice = self.get_choice()
+            out = self.handle_choice(choice)
+            if(out):
+                break
+        return out
 
 class Item:
     def __init__(self, name: str, weight: int):
